6c91b9c9eae7f7f1ffddae98c1288aac7643f934
meher12 <meher.khemiri@gmail.com>
2022-01-17 16:11:17 +0100

    SpringBootFreeMarke

---
+++ src/main/java/guru/springfreemarker/SpringfreemarkerApplication.java
@@ -0,0 +1,13 @@
+package guru.springfreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfreemarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfreemarkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springfreemarker/SpringfreemarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springfreemarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfreemarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
